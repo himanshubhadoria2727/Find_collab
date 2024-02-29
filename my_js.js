068d690c5ce79b9f8e296edf9a2a65b12ed13e08
himanshubhadoria2727 <hbhadoria78@gmail.com>
2024-02-29 23:52:03 +0530

    youtube

--- my_js.js
+++ my_js.js
@@ -3,11 +3,10 @@ const social = document.querySelector(".social")
 function update_insta(){
     document.addEventListener("click",(e)=>{
         e.preventDefault();
-        document.querySelector(".youtube1").style.display="none"
-        document.querySelector(".instagram1").style.display="flex"
-
+        document.querySelector(".youtube1").style.display="none";
+        document.querySelector(".instagram1").style.display="flex";
     })
-}
+}   
 
 function update_youtube(){
     document.addEventListener("click",(e)=>{
