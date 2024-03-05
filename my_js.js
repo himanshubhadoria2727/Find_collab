424423ac7518ed8539b2169046d770b8725bc140
himanshubhadoria2727 <hbhadoria78@gmail.com>
2024-03-05 18:19:12 +0530

    blog page update

--- my_js.js
+++ my_js.js
@@ -4,6 +4,8 @@ function update_insta(){
     document.addEventListener("click",(e)=>{
         e.preventDefault();
         document.querySelector(".youtube1").style.display="none";
+        document.querySelector(".blog1").style.display="none";
+        
         document.querySelector(".instagram1").style.display="flex";
     })
 }   
@@ -11,7 +13,19 @@ function update_insta(){
 function update_youtube(){
     document.addEventListener("click",(e)=>{
         e.preventDefault();
-        document.querySelector(".instagram1.shady").style.display='none';
-        document.querySelector(".youtube1").style.display="flex"
+        document.querySelector(".instagram1").style.display='none';
+        document.querySelector(".blog1").style.display='none';
+
+        document.querySelector(".youtube1").style.display="flex";
+    })
+}   
+    
+function update_blog(){
+    document.addEventListener("click",(e)=>{
+        e.preventDefault();
+        document.querySelector(".instagram1").style.display="none";
+        document.querySelector(".youtube1").style.display="none"
+
+        document.querySelector(".blog1").style.display="flex";
     })
 }
